72711775c3a54fb9f4a5b6b99fa0ed110e6d1d9b
Spring2019 <Spring2019@Henrys-MacBook-Pro.local>
2019-03-05 11:03:01 -0500

    change tab directory and re-add basic leaflet map to map tab

---
+++ src/Components/Tabs/EventTab.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EventTab extends Component {
+    render() {
+      return (
+        <h1>Events</h1>
+      );
+    }
+}
+
+export default EventTab;
---
+++ src/Components/Tabs/SignUpTab.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignUpTab extends Component {
+    render() {
+      return (
+        <h1>Sign Up</h1>
+      );
+    }
+}
+
+export default SignUpTab;
