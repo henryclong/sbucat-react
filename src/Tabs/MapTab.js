afea5a527e221a9ebce3319fe42f48b1a940abd8
Spring2019 <Spring2019@Henrys-MacBook-Pro.local>
2019-03-05 01:21:08 -0500

    add a basic leaflet map to the map tab

--- src/Tabs/MapTab.js
+++ src/Tabs/MapTab.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const position = [40.9124, -73.1234];
 
 class MapTab extends Component {
-    render() {
-      return (
-        <h1>Map</h1>
-      );
-    }
+  render() {
+    return (
+      <Map className='Map' center={position} zoom={15}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        <Marker position={position}>
+          <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+        </Marker>
+      </Map>
+    );
+  }
 }
 
 export default MapTab;
