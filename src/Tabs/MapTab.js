cc29dd82475b4ff70169d7cf5e043d8ff0bca5c3
Spring2019 <Spring2019@Henrys-MacBook-Pro.local>
2019-03-05 10:23:12 -0500

    update coordinates of leaflet map

--- src/Tabs/MapTab.js
+++ src/Tabs/MapTab.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 
-const position = [40.9124, -73.1234];
+const position = [40.9154, -73.1234];
 
 class MapTab extends Component {
   render() {
     return (
-      <Map className='Map' center={position} zoom={15}>
+      <Map className='Map' center={position} zoom={14.5}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
-        <Marker position={position}>
+        <Marker className='Marker' position={position}>
           <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
         </Marker>
       </Map>
